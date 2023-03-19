69906d07a0f476f01ea268d7e2a50f96411b2ee3
Luis Pedro <101891870+luis-pedro@users.noreply.github.com>
2023-03-18 21:37:10 -0300

    aula 04

---
+++ aula05/js/scripts.js
@@ -0,0 +1,24 @@
+console.log("Primeiro");
+
+console.log("Último");
+
+console.LOG("Mais um console.log");
+
+// Declara uma variável
+var a = 1;
+
+a = 'teste'
+
+console.log(a)
+
+// Este é um comentário
+
+/*
+    Este
+    é
+    um
+    comentário
+    de
+    múltiplas
+    linhas
+*/
