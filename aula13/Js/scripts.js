54f944d97d929fc61c7747dbc6e996be40ad2387
Luis Pedro <101891870+luis-pedro@users.noreply.github.com>
2023-05-17 11:14:18 -0300

    aula 13

---
+++ aula13/Js/scripts.js
@@ -0,0 +1,26 @@
+var idade = 18;
+var nome = "Luis";
+
+if(idade >= 18) {
+    console.log("O usuário pode fazer a carteira");
+}
+
+if(idade <= 17) {
+    console.log("O usuário não pode fazer a carteira");
+}
+
+if(nome == "Luis") {
+    console.log("Prazer Luis");
+}
+
+if(nome !== "Luis") {
+    console.log("Você não é o Luis");
+}
+
+if (20 > 10) {
+    console.log("Passou");
+}
+
+if (100 < 20) {
+    console.log("Passou");
+}
