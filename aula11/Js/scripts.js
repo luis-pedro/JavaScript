70db0d3394a394a4010efb33851446830e0d8e47
Luis Pedro <101891870+luis-pedro@users.noreply.github.com>
2023-05-17 10:38:02 -0300

    aula 11

---
+++ aula11/Js/scripts.js
@@ -0,0 +1,16 @@
+var arr = [16, "Luis", true, {teste: 1,teste: 2,}]; //obj -> {}
+
+console.log(arr);
+
+var arr2 = [2,3,4,5,6];
+
+console.log(arr[1]);
+console.log(arr[0]);
+
+console.log(arr2);
+
+arr2[5] = 7;
+
+console.log(arr2);
+
+console.log(typeof arr);
