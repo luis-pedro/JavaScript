f16b6a688beef0564a8af8f5099bea0839b27aef
Luis Pedro <101891870+luis-pedro@users.noreply.github.com>
2023-05-17 10:57:37 -0300

    Aula 12

---
+++ aula12/Js/scrips.js
@@ -0,0 +1,13 @@
+var idade = 19;
+
+console.log("antes do if");
+
+if(idade > 18) {
+    console.log("pode fazer a carteira de habilitação");
+}
+
+if(idade > 15) {
+    console.log("Precisa esperar 3 anos ainda");
+}
+
+console.log("depois do if");
