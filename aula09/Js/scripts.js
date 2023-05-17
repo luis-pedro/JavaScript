d1e3a7c670a7bbaaadb87e675ff0a1db8eef13df
Luis Pedro <101891870+luis-pedro@users.noreply.github.com>
2023-05-17 09:06:16 -0300

    Aula 09

---
+++ aula09/Js/scripts.js
@@ -0,0 +1,14 @@
+// Hoisting - içamento
+var numero =  5;
+console.log(sobrenome);
+
+var nome = null;
+var sobrenome = "Pedro";
+
+console.log(nome);
+console.log(sobrenome);
+
+nome = "Luis";
+
+console.log(nome);
+console.log(numero);
