c8d2f17305940ee6cc6ee8c6ceccc310f52ad372
Luis Pedro <101891870+luis-pedro@users.noreply.github.com>
2023-05-29 20:56:49 -0300

    aula 15 e 16

---
+++ aula16/Js/scripts.js
@@ -0,0 +1,14 @@
+var idade = 16;
+var nome = "Luis";
+
+if (nome = "Luis" && idade = 16) {
+    console.log("Luis pode entrar na aula de esgrima");
+} else {
+    console.log("Este não é o Luis");
+}
+
+if ((1 === 1) && (3 > 2) && (true)) {
+    console.log("passou");
+} else if((nome === "João") && (idade >= 14)) {
+    console.log("Aqui passa!");
+}
