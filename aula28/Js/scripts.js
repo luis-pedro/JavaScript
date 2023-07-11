9d9b31b78951f8278a68796d2e7077a9568f6e78
Luis Pedro <101891870+luis-pedro@users.noreply.github.com>
2023-07-11 18:08:31 -0300

    aula 28

---
+++ aula28/Js/scripts.js
@@ -0,0 +1,25 @@
+//length
+
+var nome = 'Luis';
+
+console.log(nome.length);
+
+var obj = 'fórmula 1'
+
+console.log(obj.length);
+
+//indexof
+
+console.log(nome[2]);
+
+var frase = 'O rato roeu a roupa do rei de Roma';
+
+console.log(frase.indexOf("roeu"));
+
+var roeu = frase.slice(7, 11);
+
+console.log(roeu);
+
+var novaFrase = frase.replace("roeu", "teste");
+
+console.log(novaFrase);
