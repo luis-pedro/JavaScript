e9ba6442e59bf008a7a0d633d1819e743f9124f9
Luis Pedro <101891870+luis-pedro@users.noreply.github.com>
2023-07-14 15:38:47 -0300

    aula 32

---
+++ aula32/Js/scripts.js
@@ -0,0 +1,18 @@
+let pessoa = {
+    nome: 'Luis',
+    idade: 16,
+    falar: function() {
+        console.log("Olá, tudo bem?");
+    },
+    soma: function(a, b) {
+        return a + b;
+    },
+};
+
+console.log(pessoa.nome);
+
+pessoa.falar();
+
+var soma = pessoa.soma(2, 2);
+
+console.log(soma);
