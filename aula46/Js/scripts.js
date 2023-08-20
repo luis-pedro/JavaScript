97d7ab4579c6e6e12f90dfa26f35653c942cb3e6
Luis Pedro <101891870+luis-pedro@users.noreply.github.com>
2023-08-20 18:38:53 -0300

    aula 46

---
+++ aula46/Js/scripts.js
@@ -0,0 +1,13 @@
+//setTimeout
+console.log("Antes do setTimeout");
+
+setTimeout(function() {
+    console.log("Testando o setTimeout");
+}, 2000);
+
+console.log("Após o setTimeout");
+
+//setInterval
+setInterval(function() {
+    console.log("Testando o setInterval");
+}, 1000);
