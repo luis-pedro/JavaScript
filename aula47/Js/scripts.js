8dcdf9cc643e67e2eef44407133634d05fe91479
Luis Pedro <101891870+luis-pedro@users.noreply.github.com>
2023-08-20 18:50:42 -0300

    aula 47

---
+++ aula47/Js/scripts.js
@@ -0,0 +1,23 @@
+//clearTimeout na prática
+var x = 0;
+
+var myTimer = setTimeout(function() {
+    console.log("O x é 0");
+}, 1500);
+
+x = 5;
+
+if(x > 0) {
+    clearTimeout(myTimer);
+    console.log("O x passou de 0")
+}
+
+//clearSetInterval na prática
+var myInterval = setInterval(function() {
+    console.log("Imprimindo Interval");
+}, 500);
+
+setTimeout(function() {
+    console.log("Não precisamos mais repetir!");
+    clearInterval(myInterval);
+}, 1500);
